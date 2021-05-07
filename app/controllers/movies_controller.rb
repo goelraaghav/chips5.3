984f55f1d8c3622ab68705b464a3a51bcb580c0e
Raaghav Goel <rgoel@berkeley.edu>
2021-05-07 09:34:45 +0000

    part 3 not working properly

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,11 +8,63 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    @sort = params[:sort]
-    @ratings_to_show = setup_ratings_to_show_array
-    @ratings_hash = @ratings_to_show.each_with_object({}) { |k, h| h[k] = 1 }
-    @movies = Movie.with_ratings(setup_ratings_to_show_array).order(@sort)
+    @sort = params[:sort] || session[:sort]
+    @ratings_to_show = setup_ratings_to_show_array.each_with_object({}) { |k, h| h[k] = 1 } || session[:ratings]
+    @movies = Movie.with_ratings(@ratings_to_show.keys).order(@sort)
+    session[:sort], session[:ratings] = @sort, @ratings_to_show
+    
+    
+    if params[:sort].nil? && params[:ratings].nil?
+      session.clear
+#     elsif session[:sort] || session[:ratings]
+#       redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
+    end
   end
+  
+  
+#   def index
+#     # Deal with movie ratings now...
+#     @all_ratings = Movie.get_possible_ratings
+
+#     # If user has specified 1 or more ratings, then update session ratings
+#     unless params[:ratings].nil?
+#       @filtered_ratings = params[:ratings]
+#       session[:filtered_ratings] = @filtered_ratings
+#     end
+
+#     # If the user has specified a sorting mechanism, update session sorting mechanism
+#     if params[:sorting_mechanism].nil?
+#       # If user didn't specify a sorting mechanism, then we're going to sort by the
+#       # sorting mechanism in our sessions
+#     else
+#       session[:sorting_mechanism] = params[:sorting_mechanism]
+#     end
+
+#     if params[:sorting_mechanism].nil? && params[:ratings].nil? && session[:filtered_ratings]
+#       @filtered_ratings = session[:filtered_ratings]
+#       @sorting_mechanism = session[:sorting_mechanism]
+#       flash.keep
+#       redirect_to movies_path({order_by: @sorting_mechanism, ratings: @filtered_ratings})
+#     end
+
+#     @movies = Movie.all
+
+#     # Filter movies based on rating if our sessions hash has any ratings in it
+#     if session[:filtered_ratings]
+#       @movies = @movies.select{ |movie| session[:filtered_ratings].include? movie.rating }
+#     end
+
+#     # title_sort symbol was placed in the params
+#     if session[:sorting_mechanism] == "title"
+#       @movies = @movies.sort! { |a,b| a.title <=> b.title }
+#       @movie_highlight = "hilite"
+#     elsif session[:sorting_mechanism] == "release_date"
+#       @movies = @movies.sort! { |a,b| a.release_date <=> b.release_date }
+#       @date_highlight = "hilite"
+#     else
+      
+#     end
+#   end
 
   def new
     # default: render 'new' template
